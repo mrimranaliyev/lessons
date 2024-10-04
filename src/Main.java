2cd23060f2583ef005634fef4b2ba2bc5ae22385
mrimranaliyev <imranalyev237@gmail.com>
2024-10-04 16:35:02 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Oğlanların sayını daxil edin");
+
+    int a= scanner.nextInt();
+    int b = a - 3;
+    int c = a + b;
+
+    System.out.println("qizlarin sayı: " + b);
+
+    System.out.println("ümumi şagird sayı: " + c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ;
+    }
+}
